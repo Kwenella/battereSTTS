81f8ee751762c67a6ed4e4d0480a8670b63ddfe9
K_R <K_R@K-Rs-MacBook-Pro.local>
2017-11-05 18:41:24 +0200

    first one

--- BatteSTTS/main.swift
+++ BatteSTTS/main.swift
@@ -8,5 +8,22 @@
 
 import Foundation
 
-print("Hello, World!")
+var battery = Battery()
+if battery.open() != kIOReturnSuccess { exit(0) }
 
+print("\n-- BATTERY --\n")
+print("\tAC POWERED:      \(battery.isACPowered())")
+print("\tCHARGED:         \(battery.isCharged())")
+print("\tCHARGING:        \(battery.isCharging())")
+print("\tCHARGE:          \(battery.charge())%")
+print("\tCAPACITY:        \(battery.currentCapacity()) mAh")
+print("\tMAX CAPACITY:    \(battery.maxCapactiy()) mAh")
+print("\tDESGIN CAPACITY: \(battery.designCapacity()) mAh")
+print("\tCYCLES:          \(battery.cycleCount())")
+print("\tMAX CYCLES:      \(battery.designCycleCount())")
+print("\tTEMPERATURE:     \(battery.temperature())°C")
+print("\tBATTERY LIFE:    \(battery.maxCapactiy() * 100/battery.designCapacity()) %")
+print("\tTIME REMAINING:  \(battery.timeRemainingFormatted())")
+print("\n\n")
+
+_ = battery.close()
